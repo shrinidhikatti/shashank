0d92b83b86da4fef292868750eda23a198cce57b
shrinidhikatti <chinnu.katti@gmail.com>
2026-02-17 00:01:45 +0530

    Fix Vercel serverless API configuration

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 // Vercel serverless function entry point
-const app = require('../server');
+const app = require('../server.js');
 
+// Export as default for Vercel
 module.exports = app;
+module.exports.default = app;
